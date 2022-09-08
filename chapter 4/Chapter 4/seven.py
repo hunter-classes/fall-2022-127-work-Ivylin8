522181a27836e2cb11d80e1ead3b5122273eb934
Ivy Lin <104948596+Ivylin8@users.noreply.github.com>
2022-09-08 04:36:29 +0000

    chapter 4

---
+++ chapter 4/Chapter 4/seven.py
@@ -0,0 +1,9 @@
+import turtle 
+
+wn = turtle.Screen()
+crush = turtle.Turtle ()
+
+crush.foward(50)
+crush.left(angle)
+for angle in [160, -43, 270, -97, -43, 200, -940, 17, -86]:
+    print("The pirate's destination", crush.heading())
