c0a6cb9e23c1fbba7bcacb406b000223fd3b3a9d
Ivy Lin <104948596+Ivylin8@users.noreply.github.com>
2022-10-27 03:15:37 +0000

    modes hw

---
+++ mode/modes.py
@@ -0,0 +1,9 @@
+list1 = [120, 23, 72, 34, 99]
+print ("list= ", list1)
+
+smallest_num = min(list1)
+print("The smallest number in the list is", smallest_num)
+
+
+alist = ["hello", "why", 89, 44, "cool"]
+print(len(alist))
