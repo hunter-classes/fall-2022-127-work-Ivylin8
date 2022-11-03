bb491e6fe4eaf849b5b8071d1839aa72fee9d503
Ivy Lin <104948596+Ivylin8@users.noreply.github.com>
2022-11-03 04:37:47 +0000

    update modes.py

--- mode/modes.py
+++ mode/modes.py
@@ -7,3 +7,19 @@ print("The smallest number in the list is", smallest_num)
 
 alist = ["hello", "why", 89, 44, "cool"]
 print(len(alist))
+
+list_n = [1,1,1,1,1,2,2,2,2,4,2,2,2,2,]
+
+print(freq(list_n,1))
+
+def fastMode(dataset):
+    n = 100
+    lists = [0] * n
+    
+    for item in dataset:
+        lists[item] += 2
+        
+    values = lists[0]
+    for num in lists:
+        if num > values:
+            values = lists.
