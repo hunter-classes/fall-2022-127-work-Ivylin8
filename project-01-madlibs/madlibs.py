5085b238f573546c5de3b13376c840ff765e8369
Ivy Lin <104948596+Ivylin8@users.noreply.github.com>
2022-11-03 23:43:03 +0000

    edited to add story txt

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -4,46 +4,59 @@
 
 from random import randint
 
-#sets up dictionary 
+#sets up dict
 word_dict = {
     '<HERO>' :['Sam', 'Nate', 'Jen', 'Carl'],
     '<LOCATION>' :['beach', 'playground', 'store', 'mall', 'school', 'basement'],
-    '<ADJECTIVE>' :['muscular', 'bewildered', 'colossal', 'witty'],
     '<NOUN>' :['bear', 'rocket', 'kangaroo', 'shark'],
     '<VERB>' : ['jumped', 'walked', 'ran', 'skipped', 'laughed']
 }
 
-story_file = "story.txt"
-result = ""
+#sets up list of special words
+special_words = ['<HERO>', '<LOCATION>', '<NOUN>', '<VERB>']
+
+
+result = ''
 
 def is_punctuated(word):
-    last_char = word[-1]
-    return (last_char == '.' or last_char == '!' or last_char == '?')
-
+    last_char = word[-1] #get the last character of a word
+    if (last_char == '.' or last_char == '!' or last_char == '?'): #last character is a punctuation mark or else
+        return True
+    else:
+        return False
 
 def remove_punctuation(word):
-    if (is_punctuated(word)):
-        return word[0:len(word) - 1]
+    if (is_punctuated(word) == True):
+        return word[0:len(word) - 1] #gets rid if there is
+    else:
+        return word 
+
+def remove_newline(word):
+    last_char = word[-1] 
+    if (last_char == '\n'): 
+        return word[0:len(word) - 1] #remove the last character if it is a newline
     else:
         return word
 
 #open story file and read the lines from it
-file = open(story_file)
-story = file.readlines()
-for line in story:
-    words = line.split(' ')
-    for word in words:
-        word_no_punct = remove_punctuation(word)
+f = open('story.txt')
+story = f.readlines()
+for line in story: 
+    words = line.split(' ') #splits into list of words 
+    for word in words: 
+        no_newline_word = remove_newline(word)
+        processed_word = remove_punctuation(no_newline_word) #remove punctuation from the word
             
-        if (word_no_punct in word_dict.keys()):
-            word_dict_entry = word_dict[word_no_punct]
+        if (processed_word in special_words): #spot special word
+            word_dict_entry = word_dict[processed_word] #uses special word
             random_index = randint(0, len(word_dict_entry) - 1)
-            result += " " + word_dict_entry[random_index]
+            result += ' ' + word_dict_entry[random_index] 
             
-            if (is_punctuated(word)):
-                result += word[-1]
+            if (is_punctuated(no_newline_word)):
+                result += no_newline_word[-1] 
         else:
-            result += " " + word
-    result += '\n'
-        
-print("madlib: " + result)
+            result += ' ' + no_newline_word 
+    result += '\n' 
+f.close() 
+
+print('madlib:\n' + result) 
