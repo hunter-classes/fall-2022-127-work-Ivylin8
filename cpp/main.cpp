bed96a5c89b1713a2fccc6baff99927fa3b8d308
Ivy Lin <104948596+Ivylin8@users.noreply.github.com>
2022-12-15 02:07:37 +0000

    update cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,8 +1,23 @@
-// Your First C++ Program
-
 #include <iostream>
+#include <string>
 
+using namespace std;
+
+//Working Functon
 int main() {
-    std::cout << "Hello World!";
-    return 0;
+  
+  int number;
+  
+  cout << "Enter an number: ";
+  cin >> number;
+
+  if (number > 0) {
+    cout << "This number is postive: " << number << endl;
+  }
+  //Base Project
+  cout << "Hello World.";
+
+  return 0;
 }
+/////////////////////////
+
